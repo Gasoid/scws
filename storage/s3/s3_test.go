711c27af34667ac1ade44dd0337605a2be11b4ee
Rinat Almakhov <gasoid@gmail.com>
2021-10-10 15:39:16 +0200

    added tests and adjusted readme

--- storage/s3/s3_test.go
+++ storage/s3/s3_test.go
@@ -7,21 +7,31 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
+const (
+	s3 = "s3"
+)
+
 func TestNew(t *testing.T) {
-	//utils.SetupTests()
 	c := config.New()
+	if c.Storage != s3 {
+		t.Skip("no aws creds, skipping tests")
+	}
 	storage, err := New(c)
 	assert.NoError(t, err)
 	assert.NotNil(t, storage)
 }
 
 func TestNewObject(t *testing.T) {
-	//utils.SetupTests()
 	c := config.New()
+	if c.Storage != s3 {
+		t.Skip("no aws creds, skipping tests")
+	}
 	storage, err := New(c)
 	assert.NoError(t, err)
 	o := storage.newObject()
 	name := "README.md"
+	assert.NotNil(t, o.store)
+	assert.NotEmpty(t, o.prefix)
 	cloudObject, err := o.getObject(name)
 	assert.NoError(t, err)
 	assert.NotNil(t, cloudObject)
